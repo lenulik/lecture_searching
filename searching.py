384a4f65c68e4d85d3bdd505f50bc0b4b5742505
Lenka Prchalova <prchaloval@email.cz>
2025-04-07 09:02:38 +0200

    faith over fear

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,12 +12,22 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+
+    if field not in {"unordered_numbers","ordered_numbers","dna_sequence"}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, "r") as json_file:
+        seq = json.load(json_file)
 
 
 def main():
-    pass
+    file_name = "sequential.json"
 
+    #read data
+    sequential_data = read_data(file_name, field="ordered_numbers")
+    print(sequential_data)
 
 if __name__ == '__main__':
     main()
